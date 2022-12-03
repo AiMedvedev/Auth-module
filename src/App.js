1cf73e9e82f02a0f251d05b1b8d490646909e977
Alex <forcablaugrana@gmail.com>
2022-12-03 20:57:45 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="options">
+          <div className="lang-choice"> Здесь будет выбор языка </div>
+          <button className="login"> ВОЙТИ </button>
+        </div>
+      </header>
+      <main className="main">
+        <div className="modal"> Здесь будет модальное окно</div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
