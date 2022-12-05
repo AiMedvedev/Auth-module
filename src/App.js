8f3355b0ba2a2759d1e54beb5078077419884485
Alex <forcablaugrana@gmail.com>
2022-12-05 11:38:48 +0300

    beginning

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
-import './App.css';
-import firebase from 'firebase/compat/app';
+//import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Register from "./components/accounts/Register/Register";
+import Login from "./components/accounts/Login/Login";
+/* import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/database';
 import {useAuth, useLoggedIn} from './components/Hooks/useAuth';
 import {Header} from './components/Header/Header';
 import {Modal} from './components/Modal/Modal';
 import {Context} from './components/Functions/context';
-import {firebaseConfig} from './firebase';
+import {firebaseConfig} from './firebase'; */
 
 
-firebase.initializeApp(firebaseConfig);
+//firebase.initializeApp(firebaseConfig);
 
 function App() {
-  const auth = useAuth(firebase.auth);
-  const isLoggedIn = useLoggedIn(null);
+  //const auth = useAuth(firebase.auth);
+  //const isLoggedIn = useLoggedIn(null);
   
 
   return (
-    <Context.Provider value={{auth, isLoggedIn}}>
-      <Header className="App-header" {...auth} />
-      <main className="main">
-        {auth.authentication ?
-        <div> Здесь будет приложение</div>
-        :
-        <Modal />
-        }
-      </main>
-    </Context.Provider>
+    <Router>
+      <Routes>
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+
+//<Context.Provider value={{auth, isLoggedIn}}>
+  //    <Header className="App-header" {...auth} />
+    //  <main className="main">
+      //  {auth.authentication ?
+        //<div> Здесь будет приложение</div>
+        //:
+        //<Modal />
+       // }
+      //</main>
+    //</Context.Provider>
