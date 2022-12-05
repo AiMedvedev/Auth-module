82f074ee2ae6ee76800905930ab65e4fb377b619
Alex <forcablaugrana@gmail.com>
2022-12-05 21:57:46 +0300

    no fetch problem

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -29,7 +29,7 @@ export default function Header() {
             Authorization: `Bearer ${token}`,
           },
         };
-        const res = await fetch("http://localhost:3000", payloadHeader);
+        const res = await fetch("auth-module-e9a14.firebaseapp.com", payloadHeader);
       } catch (e) {
         console.log(e);
       }
